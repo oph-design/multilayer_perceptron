3010d5ad5ec9d14c55d9f6d8d1800095e1554bd6
oph-design <ole.paul.heinzelmann@gmail.com>
2024-07-01 16:55:44 +0200

    add derivatives of sigmoid and bce

--- src/train_model/functions.py
+++ src/train_model/functions.py
@@ -6,6 +6,11 @@ def sigmoid(x: np.ndarray) -> np.ndarray:
     return 1 / (1 + np.exp(x * -1))
 
 
+def sigmoid_prime(x: np.ndarray) -> np.ndarray:
+    """applys sigmoid derivative on array"""
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
 def soft_max(x: np.ndarray) -> np.ndarray:
     """applys soft_max activation on array"""
     return np.exp(x) / np.sum(np.exp(x))
@@ -14,3 +19,8 @@ def soft_max(x: np.ndarray) -> np.ndarray:
 def binary_cross_entropy(y: np.ndarray, p: np.ndarray) -> np.ndarray:
     """calculates error for the current prediction"""
     return -(y * np.log(p) + (1 - y) * np.log(p))
+
+
+def bce_prime(y: np.ndarray, p: np.ndarray) -> np.ndarray:
+    """calculates error derivative for the current prediction"""
+    return (p - y) / p * (1 - p)
