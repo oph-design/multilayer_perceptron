c4107513b575a1ad8f65b71aedc6141316a192d3
oph-design <ole.paul.heinzelmann@gmail.com>
2024-07-04 17:08:41 +0200

    finish implementation for back propagation

--- src/train_model/layer.py
+++ src/train_model/layer.py
@@ -8,29 +8,33 @@ class Layer:
         """initializes  weights and biases"""
         self.biases = np.random.rand(count)
         self.weights = np.random.rand(count, prev_count)
-        self.errors = np.zeros((size, count))
         self.weighted_sums = np.zeros(count)
+        self.errors = np.zeros((size, count))
+        self.bias_delta = np.zeros(self.biases.shape)
+        self.weight_delta = np.zeros(self.weights.shape)
 
     def get_neurons(self, prev_neurons: np.ndarray) -> np.ndarray:
         """returns the activation for all neurons"""
         self.weighted_sums = np.dot(self.weights, prev_neurons) + self.biases
         return sigmoid(self.weighted_sums)
 
-    def get_weights(self) -> np.ndarray:
-        """return the current weights of the layer"""
-        return self.weights
-
     def calculate_error(
         self, post_weights: np.ndarray, post_error: np.ndarray, index: int
     ) -> np.ndarray:
         """calculate the error of each neuron of the layer"""
-        self.errors[index] = np.dot(post_weights.T, post_error) * sigmoid_prime(
-            self.weighted_sums
-        )
+        gradient = sigmoid_prime(self.weighted_sums)
+        self.errors[index] = np.dot(post_weights.T, post_error) * gradient
         return self.errors[index]
 
-    def apply_error(self, lr: float) -> None:
+    def learn(self, lr: float, prev_activation: np.ndarray, index: int) -> None:
         """adjust the parameters based on the current error"""
-        mean_errors = np.mean(self.errors, axis=1)
-        self.biases = self.biases - lr * mean_errors
-        self.weights = self.weights - lr * mean_errors
+        change_rate = lr * np.outer(self.errors[index], prev_activation)
+        self.weight_delta = self.weight_delta + change_rate
+        self.bias_delta = self.bias_delta + lr * self.errors[index]
+
+    def apply_changes(self, size: int) -> None:
+        """means the adjustments and applys them to the weights"""
+        self.biases = self.biases - self.bias_delta / size
+        self.weights = self.weights - self.weight_delta / size
+        self.bias_delta = np.zeros(self.biases.shape)
+        self.weight_delta = np.zeros(self.weights.shape)
--- src/train_model/network.py
+++ src/train_model/network.py
@@ -1,16 +1,7 @@
 import numpy as np
 import pandas as pd
 from .layer import Layer
-from .mathematics import binary_cross_entropy
-
-
-def format_array(source: np.ndarray) -> np.ndarray:
-    """creates the opposite of the source array merges both"""
-    opposite = 1 - source
-    formated = np.empty((len(source) + len(opposite),), dtype=source.dtype)
-    formated[0::2] = source
-    formated[1::2] = opposite
-    return formated
+from .mathematics import bce_prime, sigmoid_prime, sigmoid
 
 
 class Network:
@@ -22,25 +13,23 @@ class Network:
         self.rate = conf["learning_rate"]
         self.size = conf["batch_size"]
         self.dim = np.insert([30, 2], 1, conf["layer"])
-        self.count = len(self.dim)
         self.layers = [
-            Layer(self.dim[x - 1], self.dim[x], self.size) for x in range(1, self.count)
+            Layer(self.dim[x - 1], self.dim[x], self.size)
+            for x in range(1, len(self.dim))
         ]
 
     def fit(self):
         """performs gradient descent for all layers in epoch time"""
         for i in range(self.epochs):
             batch = self.data.loc[self.data["Batch"] == i % self.size].values
-            y = batch[:, 1:2].flatten()
-            x = batch[:, 2:]
-            p = self.forwardprop(x)
-            error = np.column_stack(
-                (binary_cross_entropy(y, p[:, 0]), binary_cross_entropy(1 - y, p[:, 1]))
-            )
-            self.backwardprop(error)
-            self.train_model()
+            target = batch[:, 1:2].flatten()
+            input = batch[:, 2:]
+            prediction = self.feed_forward(input)
+            error = self.calc_out_layer_error(target, prediction)
+            self.propagate_backwards(error)
+            self.adjust_parameters(input)
 
-    def forwardprop(self, batch: np.ndarray) -> np.ndarray:
+    def feed_forward(self, batch: np.ndarray) -> np.ndarray:
         """calculates the prediction for the current weights"""
         res = []
         for data in batch:
@@ -49,15 +38,27 @@ class Network:
             res.append(data)
         return np.array(res)
 
-    def backwardprop(self, errors: np.ndarray) -> None:
+    def calc_out_layer_error(self, y: np.ndarray, p: np.ndarray) -> np.ndarray:
+        """calculates the error in the output layer"""
+        gradient = sigmoid_prime(self.layers[-1].weighted_sums)
+        error_malignent = bce_prime(y, p[:, 0]) * gradient[0]
+        error_benign = bce_prime(1 - y, p[:, 1]) * gradient[1]
+        return np.column_stack((error_malignent, error_benign))
+
+    def propagate_backwards(self, errors: np.ndarray) -> None:
         """calculates errors for all layers"""
         for index, error in enumerate(errors):
-            weights = self.layers[-1].get_weights()
+            weights = self.layers[-1].weights
             for layer in self.layers[::-1][1:]:
                 error = layer.calculate_error(error, weights, index)
-                weights = layer.get_weights()
+                weights = layer.weights
 
-    def train_model(self) -> None:
-        """adjusts paramters for all layers"""
+    def adjust_parameters(self, input_activation: np.ndarray) -> None:
+        """applys learings for all the layers"""
+        for index, input in enumerate(input_activation):
+            prev_activation = input
+            for layer in self.layers:
+                layer.learn(self.rate, prev_activation, index)
+                prev_activation = sigmoid(layer.weighted_sums)
         for layer in self.layers:
-            layer.apply_error(self.rate)
+            layer.apply_changes(self.size)
